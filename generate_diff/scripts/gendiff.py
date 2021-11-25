2c8e198009f774cb1729db4b0ef6627ae14134cd
shifter <vanfre93@gmail.com>
2021-11-25 13:34:04 +0300

    add information with helpfull argparser

---
+++ generate_diff/scripts/gendiff.py
@@ -0,0 +1,6 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+args = parser.parse_args()
