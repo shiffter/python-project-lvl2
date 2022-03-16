33f79231bf32b4495691746e8e590fa5bf7cfe8c
shifter <vanfre93@gmail.com>
2022-03-16 23:15:34 +0300

    fix format

--- gendiff/scripts/gendiff_s.py
+++ gendiff/scripts/gendiff_s.py
@@ -8,11 +8,11 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff', epilog='Created by shiffter')
     parser.add_argument('file_1', type=str)
     parser.add_argument('file_2', type=str)
-    parser.add_argument('-f', '--format', default='stylish', help='set format of output', type=str)
+    parser.add_argument('-f', '--format', default=None, help='set format of output', type=str)
     args = parser.parse_args()
     path_1 = os.path.abspath(args.file_1)
     path_2 = os.path.abspath(args.file_2)
-    diff = (generate_diff(path_1, path_2, mode=args.format))
+    generate_diff(path_1, path_2, mode=args.format)
 
 
 if __name__ == '__main__':
