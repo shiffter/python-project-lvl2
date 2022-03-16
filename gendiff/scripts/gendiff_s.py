1a767d453b2a4f2b7b6d064a27b38f06affc1593
shifter <vanfre93@gmail.com>
2022-03-16 23:32:23 +0300

    refactor format

--- gendiff/scripts/gendiff_s.py
+++ gendiff/scripts/gendiff_s.py
@@ -1,17 +1,18 @@
 import argparse
 import os
 from gendiff import generate_diff
+import json
 
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff', epilog='Created by shiffter')
     parser.add_argument('file_1', type=str)
     parser.add_argument('file_2', type=str)
-    parser.add_argument('-f', '--format', default=None, help='set format of output', type=str)
+    parser.add_argument('-f', '--format', default='stylish', help='set format of output', type=str)
     args = parser.parse_args()
     path_1 = os.path.abspath(args.file_1)
     path_2 = os.path.abspath(args.file_2)
-    generate_diff(path_1, path_2, args.format)
+    generate_diff(path_1, path_2)
 
 
 if __name__ == '__main__':
